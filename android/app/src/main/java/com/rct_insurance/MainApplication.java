1c7b971a034d455652b2b58afb9ee23727e04455
dingyiming <609862262@qq.com>
2017-02-17 15:49:05 +0800

    xx

--- android/app/src/main/java/com/rct_insurance/MainApplication.java
+++ android/app/src/main/java/com/rct_insurance/MainApplication.java
@@ -4,6 +4,11 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.brentvatne.react.ReactVideoPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,7 +29,12 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new ReactNativeAudioPackage(),
+            new RNSoundPackage(),
+            new ReactVideoPackage(),
+            new RCTCameraPackage(),
+            new VectorIconsPackage()
       );
     }
   };
