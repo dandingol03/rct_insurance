69b0f177e0fd64a0d0db544b32725a315982fb60
dingyiming <609862262@qq.com>
2017-02-22 15:21:39 +0800

    xx

--- ios/rct_insurance/AppDelegate.m
+++ ios/rct_insurance/AppDelegate.m
@@ -19,7 +19,6 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   
-  
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
