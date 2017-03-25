8e3cb492cf2f1b191477db622ec43b0373587340
You Li <xqli@sdu.edu.cn>
2017-03-25 16:28:02 +0800

    x

--- App/containers/Register.js
+++ App/containers/Register.js
@@ -462,10 +462,10 @@ class Register extends Component{
 
     componentDidMount()
     {
-        // SInfo.setItem('name', 'danding',{
-        //     sharedPreferencesName:'shared_preferences',
-        //     keychainService:'app'
-        // });
+        SInfo.setItem('name', 'danding',{
+            sharedPreferencesName:'shared_preferences',
+            keychainService:'app'
+        });
 
     }
 
