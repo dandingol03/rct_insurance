c6a1779efb2893de9918ba96bf60baecc91db431
danding <1291248710@qq.com>
2017-03-27 14:38:41 +0800

    xx

--- App/reducers/WsReducer.js
+++ App/reducers/WsReducer.js
@@ -1,63 +1,30 @@
 
-import * as types from '../action/types';
 import {
-    UPDATE_PERSON_INFO,
-    UPDATE_SCORE,
-    UPDATE_CERTIFICATE
-} from '../constants/UserConstants';
+    ON_WS_RECV
+} from '../constants/WebsocketConstants';
 
-import {
-    UPDATE_TTS_TOKEN
-} from '../constants/TTSConstants';
-
+import _ from 'lodash';
 
 
 const initialState = {
-    accessToken: null,
-    auth:false,
-    personInfo:null,
-    contactInfo:null,
-    score:null,
-    ttsToken:null,
-    portrait:null
+    msg:null,
+    messages:[]
 };
 
 let user = (state = initialState, action) => {
 
     switch (action.type) {
 
-        case types.ACCESS_TOKEN_ACK:
+        case ON_WS_RECV:
 
+            var {msg}=action.payload;
+            var _messages=_.cloneDeep(state.messages);
+            _messages.push(msg);
             return Object.assign({}, state, {
-                accessToken: action.accessToken,
-                validate:action.validate,
-                auth:action.auth
+                msg: msg,
+                messages:_messages
             })
-        case UPDATE_PERSON_INFO:
-            var  {data}=action.payload;
-            return Object.assign({}, state, {
-                personInfo:data
-            })
-            break;
-        case UPDATE_SCORE:
-            var {data}=action.payload;
-            return Object.assign({}, state, {
-                score:data
-            })
-            break;
-        case UPDATE_CERTIFICATE:
-            var {username,password}=action.payload;
-            return Object.assign({}, state, {
-                username:username,
-                password:password
-            })
-            break;
-        case UPDATE_TTS_TOKEN:
-            var {ttsToken}=action.payload;
-            return Object.assign({}, state, {
-                ttsToken:ttsToken
-            })
-            break;
+
         default:
             return state;
     }
